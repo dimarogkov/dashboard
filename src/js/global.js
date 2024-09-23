711a65f213fa7ecafc11bced021d2edf2d099890
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-09-23 14:06:57 +0300

    update styles

--- src/js/global.js
+++ src/js/global.js
@@ -8,10 +8,9 @@
 import {store} from './store.js';
 const {sections, state} = store;
 
-// INIT PAGE
 const initPage = () => {
     console.log('JS load');
 };
 
 // INIT PAGE
-$(document).ready(() => initPage());
+document.addEventListener('DOMContentLoaded', initPage);
